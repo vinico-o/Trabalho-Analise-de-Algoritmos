53d2cab47f29630f415d18af4e5d2c955885c4fc
Cauan Simões <cauan.simoes.contato@gmail.com>
2025-09-05 17:50:10 -0300

    feat: QuickSort algorithm

---
+++ Ordenacao/QuickSort.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+def particao_primeiro(vetor, inicio, fim):
+    
+    pivo = vetor[inicio]  
+    i = inicio            
+    
+    for j in range(inicio + 1, fim + 1):
+
+        if vetor[j] < pivo:
+
+            i += 1
+            temp = vetor[i]
+            vetor[i] = vetor[j]
+            vetor[j] = temp
+    
+    
+    vetor[i], vetor[inicio] = vetor[inicio], vetor[i]
+
+    return i 
+
+def quickSort_primeiro(lista, inicio, fim):
+    
+    if inicio < fim:
+
+        p = particao_primeiro(lista, inicio, fim)
+
+        quickSort_primeiro(lista, inicio, p - 1)  
+        quickSort_primeiro(lista, p + 1, fim)    
+
+
+
+def particao_central(lista, inicio, fim):
+
+    meio = (inicio + fim) // 2
+    pivo = lista[meio]
+    
+    lista[meio], lista[inicio] = lista[inicio], lista[meio]
+    i = inicio
+
+    for j in range(inicio + 1, fim + 1):
+
+        if lista[j] < pivo:
+
+            i += 1
+            lista[i], lista[j] = lista[j], lista[i]
+    
+
+
+    lista[i], lista[inicio] = lista[inicio], lista[i]
+    return i
+
+def quickSort_central(lista, inicio, fim):
+    
+    if inicio < fim:
+
+        p = particao_central(lista, inicio, fim)
+
+        quickSort_central(lista, inicio, p - 1) 
+        quickSort_central(lista, p + 1, fim)    
+
+
