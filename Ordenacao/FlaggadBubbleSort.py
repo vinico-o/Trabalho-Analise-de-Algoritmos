e4da2483979263406a5a6bdde7ec7e1c2d61725a
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-09-05 16:51:14 -0300

    feat: add FlaggedBubbleSort algorithm

---
+++ Ordenacao/FlaggadBubbleSort.py
@@ -0,0 +1,13 @@
+def bubble_sort(lista):
+    n = len(lista)
+    
+    for i in range(n):
+        troca = False
+        for j in range(0, n - i - 1):
+            
+            if lista[j] > lista[j + 1]:
+                troca = True
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+            
+        if not troca:
+            break
