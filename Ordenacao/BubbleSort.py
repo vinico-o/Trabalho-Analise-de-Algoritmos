2dd58d2a08b8df9b96953900bf209cb58f6c3329
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-09-05 16:40:16 -0300

    removendo comentarios

--- Ordenacao/BubbleSort.py
+++ Ordenacao/BubbleSort.py
@@ -1,9 +1,9 @@
 def bubble_sort(lista):
     n = len(lista)
-    # Percorre todos os elementos da lista
+    
     for i in range(n):
-        # Últimos i elementos já estão no lugar
+        
         for j in range(0, n - i - 1):
-            # Troca se o elemento encontrado é maior que o próximo
+            
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
