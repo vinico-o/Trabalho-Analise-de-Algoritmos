bf29534f08b79eaf0df52e8ab0d7fdb25c4fdcc4
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-09-12 17:20:29 -0300

    feat: add system to run all algorithms

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import random
 import time
 import csv
+import matplotlib as plt
 
 #Módulo dos Algoritmos
 import Ordenacao.BubbleSort as bs
@@ -10,29 +11,36 @@ import Ordenacao.InsertionSort as ins
 import Ordenacao.MergeSort as ms
 import Ordenacao.SelectionSort as ss
 
-arquivo = "Algorithms.csv"
-
 def adicionarInformacoes(nome, tamanho, tempo):
     with open(arquivo, "a", newline="") as arq:
         escritor = csv.writer(arq)
         escritor.writerow([nome, tamanho, tempo])
 
+arquivo = "Algorithms.csv"
+tamanhos = [100, 1000, 10000]
+funcoes = {
+    "BubbleSort": bs.BubbleSort,
+    "FlaggedBubbleSort": fbs.FlaggedBubbleSort,
+    "InsertionSort": ins.InsertionSort,
+    "MergeSort": ms.MergeSort,
+    "SelectionSort": ss.selection_sort
+}
+
 with open(arquivo, "w", newline="") as arq:
     escritor = csv.writer(arq)
-    escritor.writerow(["Algoritmo", "Tamanho", "Tempo"])
+    #Algoritmo = nome do algoritmo de ordenacao
+    #Tamanho = tamanho do vetor utilizado na ordenacao
+    #Tempo = Tempo de execução do Algoritmo
+    #Tipo = Tipo do vetor: Crescente, Aleatorio, Decrescente
+    escritor.writerow(["Algoritmo", "Tamanho", "Tempo"]) #TODO: adicionar Tipo
 
-lista = random.sample(range(1, 10001), 10000)
+for nome, funcao in funcoes.items():
+    for tamanho in tamanhos:
+        lista = random.sample(range(1, tamanho + 1), tamanho)
+        inicio = time.time()
+        funcao(lista)
+        fim = time.time()
+        tempo = fim - inicio
+        adicionarInformacoes(nome, tamanho, f"{tempo:.2f}")
 
-inicio = time.time()
-ms.MergeSort(lista)
-fim = time.time()
-tempo = fim - inicio
-adicionarInformacoes("MergeSort", 10000, f"{tempo:.2f}")
-
-inicio = time.time()
-bs.BubbleSort(lista)
-fim = time.time()
-tempo = fim - inicio
-adicionarInformacoes("BubbleSort", 10000, f"{tempo:.2f}")
-
-print(lista)
+print("Arquivo Finalizado!")
